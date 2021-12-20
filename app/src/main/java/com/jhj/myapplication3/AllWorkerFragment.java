14863153a682d3097a634186652d8d939d9fcf32
defendo <xiustar7@gmail.com>
2021-12-20 14:16:29 +0900

    1st

---
+++ app/src/main/java/com/jhj/myapplication3/AllWorkerFragment.java
@@ -0,0 +1,44 @@
+package com.jhj.myapplication3;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+
+public class AllWorkerFragment extends Fragment {
+
+    private ListView listview_worker;
+    private ArrayList<String> al = new ArrayList<>();
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+
+
+
+
+//        listview_worker=(ListView) getView().findViewById(R.id.listview_worker);
+
+
+        /*ArrayAdapter<String> adapter =
+                new ArrayAdapter<String>(getActivity(),R.layout.all_worker_list,al);
+        listview_worker.setAdapter(adapter);*/
+
+
+
+
+
+
+        return inflater.inflate(R.layout.fragment_all_worker, container, false);
+    }
+}
